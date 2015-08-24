f987862bea869923456530d99a8faf26bed0ba61
Fabrice Kordon <Fabrice.Kordon@lip6.fr>
2015-08-24 19:28:22 +0200

    header GPL3

--- lib/benchkit.lib.sh
+++ lib/benchkit.lib.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+################################################################################
+#
+#    Contact            : Fabrice Kordon (Fabrice.Kordon@lip6.fr)
+#                         Francis Hulin-Hubard (fhh@lsv.ens-cachan.fr)
+#    Web                : https://github.com/fkordon/BenchKit
+#    Sources            : https://github.com/fkordon/BenchKit
+#    Description        : "BenchKit" is a system to massively invoke
+#    programs to be benchmarked and evaluated. It handles their execution
+#    in virtual machines and smaple times, CPU and I/O consuption.
+#    Licence            : GPL3
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+################################################################################
+
 # {{{ Initializing parameters 
 ################################################################################
 # Initializing parameters
